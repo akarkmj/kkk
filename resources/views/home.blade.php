054dd40b5ebf77e7b1c307e789beefbf2c8b8915
kkk <https://github.com/akarkmj/kkk.git>
2022-04-03 21:05:31 +0300

    akar

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>home</title>
+</head>
+<body>
+
+<a href="/profile"> Profile</a>
+<a href="/profile"> biography</a>
+
+
+
+</body>
+</html>
